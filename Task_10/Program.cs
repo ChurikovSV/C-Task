864e235724a639524b0f6fe1bee8cd7100bf6c70
churikov-sv <churikov.sv@outlook.com>
2023-09-29 21:40:19 +0300

    add Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Enter a number from 100 to 999: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1000 && number > 99)
+{
+    int Digit(int num)
+  {
+      int fDigit = num / 10;
+      int sDigit = fDigit % 10;
+      return sDigit;
+  } 
+  int result = Digit(number);
+  Console.WriteLine($"number = {result}");
+
+}
+else 
+{
+  Console.WriteLine($"You entered an incorrect number");
+}
